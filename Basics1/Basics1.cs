35d2fb1e1671347c341f6a3a3b7514d8d50fe9c4
Juan Galvis <Galvis>
2024-12-25 19:41:36 -0500

    Agregar archivos de proyecto.

---
+++ Basics1/Basics1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics1
+{
+    internal class Basics1
+    {
+        static void Main(string[] args)
+        {
+            int ladoA;
+            int ladoB;
+            int resultado;
+
+            Console.WriteLine("Calcule el área de un rectángulo ingresando los valores de los lados");
+            Console.WriteLine("Digite el valor del lado 1: ");
+            ladoA = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite el valor del lado 2: ");
+            ladoB = Convert.ToInt32(Console.ReadLine());
+
+            resultado = ladoA * ladoB;
+
+            Console.WriteLine("El área del rectangulo es: " + resultado);
+            Console.ReadLine();
+        }
+    }
+}
