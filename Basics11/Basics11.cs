b242e1d0ed5c9a47fddcf99e75fb34d005548a9d
Juan Galvis <Galvis>
2024-12-31 10:27:10 -0500

    new project added

---
+++ Basics11/Basics11.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics11
+{
+    internal class Basics11
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
