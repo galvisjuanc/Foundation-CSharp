a2892a70f87596e5f18e2a36985f9aa4cff2548a
Juan Galvis <Galvis>
2025-01-02 11:35:12 -0500

    POO ejemplo 1

---
+++ POO1/Apuntador.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO1
+{
+    class Apuntador
+    {
+        private string Color;
+        private double Largo;
+        private short NumeroDeBotones;
+        private bool TieneStickers;
+    }
+}
---
+++ POO1/POO1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO1
+{
+    internal class POO1
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
