073b2a3763b3104ac112b93e8974bea2e41a92fa
Juan Galvis <Galvis>
2025-01-03 18:32:04 -0500

    Add info for the new object Superheroe

--- POO1/POO1.cs
+++ POO1/POO1.cs
@@ -10,7 +10,14 @@ namespace POO1
     {
         static void Main(string[] args)
         {
-            
+            SuperHeroe superHeroe = new SuperHeroe();
+
+            superHeroe.Id = 1;
+            superHeroe.Nombre = "Spiderman";
+            superHeroe.IdentidadSecreta = "Peter Parker";
+            superHeroe.Ciudad = "New York";
+            superHeroe.PuedeVolar = false;
+            superHeroe.SuperPoderes = new[] { "Lanzar telaraña", "Super fuerza" };
         }
     }
 }
