c65c36aaf5513f48b00979ac9064e7bd93c9cb97
Juan Galvis <Galvis>
2025-01-07 15:24:09 -0500

    Use SuperHeroeRecord with .Net instead of .Net Framework

---
+++ POO1WITHDOTNET/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+SuperHeroeRecord superHeroeRecord = new(1, "Superman", "Clark Kent");
+SuperHeroeRecord superHeroeRecord2 = new(1, "Superman", "Clark Kent");
+
+Console.WriteLine(superHeroeRecord == superHeroeRecord2);
+
+
+public record SuperHeroeRecord(int Id, string Nombre, string IdentidadSecreta);
