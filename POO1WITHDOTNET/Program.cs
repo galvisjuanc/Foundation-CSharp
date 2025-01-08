5e63f1651e11d70e9326c152daac82e840c93124
Juan Galvis <Galvis>
2025-01-08 11:34:02 -0500

    Organize classes in internal classes II

--- POO1WITHDOTNET/Program.cs
+++ POO1WITHDOTNET/Program.cs
@@ -1,9 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
 
+using POO1WITHDOTNET.Models;
+
 SuperHeroeRecord superHeroeRecord = new(1, "Superman", "Clark Kent");
 SuperHeroeRecord superHeroeRecord2 = new(1, "Superman", "Clark Kent");
 
 Console.WriteLine(superHeroeRecord == superHeroeRecord2);
+
+var poderVolar = new SuperPoder();
+poderVolar.Nombre = "Volar";
+poderVolar.Descripcion = "Capacidad para volar y planear en el aire";
+poderVolar.Nivel = NivelPoder.NivelUno;
 
 public record SuperHeroeRecord(int Id, string Nombre, string IdentidadSecreta);
