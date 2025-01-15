d8ef2e35e3af2abf10f84fdf1aadb197945a6c5e
Mauricio Coral <Coral>
2025-01-15 10:20:31 -0500

    Use Interface in the SuperHeroe class

--- POO1WITHDOTNET/Models/SuperHeroe.cs
+++ POO1WITHDOTNET/Models/SuperHeroe.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using POO1WITHDOTNET.Interfaces;
 
 namespace POO1WITHDOTNET.Models
 {
-    internal class SuperHeroe : Heroe
+    internal class SuperHeroe : Heroe, ISuperHeroe
     {
         public int Id { get; set; }
         public override string Nombre
