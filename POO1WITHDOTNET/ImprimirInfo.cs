931b41d419b7dbbda07744f9fe07446c53a5e0fb
Mauricio Coral <Coral>
2025-01-15 10:26:36 -0500

    Class added to show superhero info

---
+++ POO1WITHDOTNET/ImprimirInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using POO1WITHDOTNET.Interfaces;
+
+namespace POO1WITHDOTNET
+{
+    internal class ImprimirInfo
+    {
+        public void ImprimirSuperHeroe(ISuperHeroe superHeroe)
+        {
+            Console.WriteLine($"ID: {superHeroe.Id}");
+            Console.WriteLine($"Nombre: {superHeroe.Nombre}");
+            Console.WriteLine($"Identidad Secreta: {superHeroe.IdentidadSecreta}");
+        }
+    }
+}
