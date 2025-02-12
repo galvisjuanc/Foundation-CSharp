07d1b7124f6d26938ffc2e580a9e4b0f45d71312
Juan Galvis <Galvis>
2025-02-12 09:51:18 -0500

    Class to show info of the books

---
+++ curso-linq/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curso_linq
+{
+    internal class Book
+    {
+        public string Title { get; set; }
+        public int PageCount { get; set; }
+        public string Status { get; set; }
+        public DateTime PublishedDate { get; set; }
+    }
+}
