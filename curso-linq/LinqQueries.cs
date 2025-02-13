c9cca5e5a9f797dc27b874a3e053a04288d24482
Juan Galvis <Galvis>
2025-02-13 08:57:28 -0500

    Refactor III over LinqQueries class

--- curso-linq/LinqQueries.cs
+++ curso-linq/LinqQueries.cs
@@ -12,11 +12,9 @@ namespace curso_linq
 
         public LinqQueries()
         {
-            using (StreamReader reader = new StreamReader("books.json"))
-            {
-                string json = reader.ReadToEnd();
-                this.librosCollection = System.Text.Json.JsonSerializer.Deserialize<List<Book>>(json, new System.Text.Json.JsonSerializerOptions() {PropertyNameCaseInsensitive = true})!;
-            }
+            using StreamReader reader = new StreamReader("books.json");
+            string json = reader.ReadToEnd();
+            this.librosCollection = System.Text.Json.JsonSerializer.Deserialize<List<Book>>(json, new System.Text.Json.JsonSerializerOptions() { PropertyNameCaseInsensitive = true })!;
         }
 
         public IEnumerable<Book> TodaLaColeccion()
