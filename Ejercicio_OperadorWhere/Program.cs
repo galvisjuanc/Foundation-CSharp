d5db2f5da730cec5697304d8fb09ca39a3c54937
Mauricio Coral <Coral>
2025-02-17 09:39:09 -0500

    Ejercicio 1 Where with LINQ

---
+++ Ejercicio_OperadorWhere/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+List<Animal> animales = new List<Animal>();
+animales.Add(new Animal() { Nombre = "Hormiga", Color = "Rojo" });
+animales.Add(new Animal() { Nombre = "Lobo", Color = "Gris" });
+animales.Add(new Animal() { Nombre = "Elefante", Color = "Gris" });
+animales.Add(new Animal() { Nombre = "Pantegra", Color = "Negro" });
+animales.Add(new Animal() { Nombre = "Gato", Color = "Negro" });
+animales.Add(new Animal() { Nombre = "Iguana", Color = "Verde" });
+animales.Add(new Animal() { Nombre = "Sapo", Color = "Verde" });
+animales.Add(new Animal() { Nombre = "Camaleon", Color = "Verde" });
+animales.Add(new Animal() { Nombre = "Gallina", Color = "Blanco" });
+
+// Escribe tu código aquí
+// filtra todos los animales que sean de color verde que su nombre inicie con una vocal
+
+public class Animal
+{
+    public string Nombre { get; set; }
+    public string Color { get; set; }
+}
