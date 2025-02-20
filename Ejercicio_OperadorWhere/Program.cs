4a036f32cdf18b2b1256bfe506c73efafb3fc29c
Mauricio Coral <Coral>
2025-02-20 09:33:27 -0500

    Remove useless info

--- Ejercicio_OperadorWhere/Program.cs
+++ Ejercicio_OperadorWhere/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-var animales = new List<Animal>
+﻿var animales = new List<Animal>
 {
     new Animal() { Name = "Hormiga", Color = "Rojo" },
     new Animal() { Name = "Lobo", Color = "Gris" },
